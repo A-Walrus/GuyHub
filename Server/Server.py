b62dbd542db6ffecea414044da37f079db09f530
Guy <Ofek.r.guy@gmail.com>
2021-04-20 19:23:48 +0300

    auth cleanup

--- Server/Server.py
+++ Server/Server.py
@@ -5,19 +5,26 @@ from flask_httpauth import HTTPBasicAuth
 from werkzeug.serving import WSGIRequestHandler
 from zipfile import ZipFile
 
+class Auth(HTTPBasicAuth):
+	def name(self):
+		return self.current_user()
+
+	def id(self):
+		return db.get_user(self.current_user())["id"]
+
 db = Database.Db("GuyHub.db")
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
+auth = Auth()
 
 app.config["commits"]=os.path.join(os.getcwd(),"commits")
+
 
 def user_access_to_repo(username,repo_id):
 	if db.user_has_access(username,repo_id):
 		return True
 	else:
 		abort(403)
-
 
 @auth.verify_password
 def verify_password(username,password):
@@ -27,14 +34,14 @@ def verify_password(username,password):
 @app.route('/profile')
 @auth.login_required
 def profile():
-	return {"user":db.get_user(auth.current_user()),"repos":db.get_user_repos(auth.current_user())}
+	return {"user":db.get_user(auth.name()),"repos":db.get_user_repos(auth.name())}
 
 
 @app.route('/create_repo', methods = ['POST'])
 @auth.login_required
 def create_repo():
 	name = request.args.get("Name")
-	db.add_repo(db.get_user(auth.current_user())["id"],name)
+	db.add_repo(auth.id(),name)
 	archive_name = os.path.join(app.config["commits"],"%s.zip"%db.get_newest_commit_id())
 	with ZipFile(archive_name, 'w') as file:
 	  pass
@@ -43,7 +50,7 @@ def create_repo():
 @app.route("/repos/<int:repo_id>")
 @auth.login_required
 def repo(repo_id):
-	if user_access_to_repo(auth.current_user(),repo_id):
+	if user_access_to_repo(auth.name(),repo_id):
 		return {"commits":db.get_commits("Repos.ID = %s"%repo_id),"repo":db.get_repo(repo_id),"branches":db.get_repo_branches(repo_id),"users":db.get_repo_users(repo_id)}
 
 @app.route("/add_user", methods = ['POST'])
@@ -51,7 +58,7 @@ def repo(repo_id):
 def add_user():
 	repo_id = request.args.get("Repo")
 	user_id = request.args.get("User")
-	if user_access_to_repo(auth.current_user(),repo_id):
+	if user_access_to_repo(auth.name(),repo_id):
 		db.add_user_to_repo(user_id,repo_id)
 		return ""
 
@@ -73,9 +80,9 @@ def fork():
 	branch_name = request.args.get("Branch")
 	commit = db.get_commits("Commits.Id = %s"%commit_id)[0]
 	repo_id = commit["repo"]["id"]
-	if user_access_to_repo(auth.current_user(),repo_id):
-		branch = db.add_branch(branch_name,db.get_user(auth.current_user())["id"],repo_id)
-		db.add_commit("Fork","",branch,commit_id,db.get_user(auth.current_user())["id"])
+	if user_access_to_repo(auth.name(),repo_id):
+		branch = db.add_branch(branch_name,auth.id(),repo_id)
+		db.add_commit("Fork","",branch,commit_id,auth.id())
 		id = db.get_newest_commit_id()
 
 		with open(os.path.join(app.config["commits"],"%s.zip"%commit_id),'rb')as r:
@@ -95,11 +102,11 @@ def get_users():
 def commit(commit_id):
 	commit = db.get_commits("Commits.Id = %s"%commit_id)[0]
 	repo_id = commit["repo"]["id"]
-	if user_access_to_repo(auth.current_user(),repo_id):
+	if user_access_to_repo(auth.name(),repo_id):
 		if request.method == 'POST': # post
 			args = request.args
 			if db.is_commit_head_of_branch(commit_id,args.get("Branch")):
-				db.add_commit(args.get("Name"),args.get("Message"),args.get("Branch"),commit_id,db.get_user(auth.current_user())["id"])
+				db.add_commit(args.get("Name"),args.get("Message"),args.get("Branch"),commit_id,db.get_user(auth.name())["id"])
 				id = db.get_newest_commit_id()
 				file = request.files["file"]
 				file.save(os.path.join(app.config["commits"],"%s.zip"%id))
